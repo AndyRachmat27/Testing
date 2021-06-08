3aa0fa033bd0c2d3928bbecfa9d45c7b13e9c87d
AndyRachmat27 <80530325+AndyRachmat27@users.noreply.github.com>
2021-06-08 22:11:42 +0700

    Update coba1.php

--- coba1.php
+++ coba1.php
@@ -1,28 +1,25 @@
 <?php
-echo "//selection sort<br>";
+echo "//selection sortn", "<br>";
 $data=array(6,5,3,1,8,7,2,4);
-function selection_sort($data){
-print_r($data); 
-echo "<br>";
+print_r($data);
+function selection_sort($data)
+{
   $n=count($data);
-  echo "jumlah data :".$n."<br>";
-
-  for ($i = 0;$i<$n;$i++){
-      $k = $i; 
-    echo "<br>";
-    echo "perulangan : ".$i."<br>";  
-    for ($j = $i+1;$j<$n;$j++){
-          if ($data[$j] < $data[$k]) $k = $j;
-    } 
-              echo "data ke: ".$i. " : ".$data[$i]." < "."data ke: ".($i+1). " : ".$data[$i+1]." = tukar";
-              $dummy=$data[$i];
-              $data[$i]=$data[$k];
-              $data[$k]=$dummy;
-              echo "<br>";          
-            }
-            return $data;
-      } 
-      
-      print_r($data);
-      echo implode (selection_sort($data));
-?>
+  for ($i = 0;$i<7 ;$i++)
+  {
+    $k = $i;
+    for ($j = $i+1;$j<$n;$j++)
+    {
+      if ($data[$j] < $data[$k]) $k = $j;
+    }
+    echo "<br>", "Perulangan = ", $i, "<br>";
+    print_r($data);
+    echo "<br>", "Mengambil Data Ke ", $k, " = ", $data[$k], " Ditukar dengan Data ke ",$i, " = ", $data[$i],  "<br>";
+    $dummy=$data[$i];
+    $data[$i]=$data[$k];
+    $data[$k]=$dummy;
+  }
+  return $data;
+}
+echo "<br>";
+print_r(selection_sort($data));
