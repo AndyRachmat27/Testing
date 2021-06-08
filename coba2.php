eeaa440403ab5f014d49111452e403f8eba60f72
AndyRachmat27 <80530325+AndyRachmat27@users.noreply.github.com>
2021-06-08 22:10:36 +0700

    Update coba2.php

--- coba2.php
+++ coba2.php
@@ -13,7 +13,7 @@ echo "<br>";
     for ($k = $i; $k>0; $k--) {
           if ($data[$k] < $data[$k-1])
           { 
-              echo "data ke: ".$k. " : ".$data[$k]." < "."data ke: ".($k-1). " : ".$data[$j-1]." = tukar";
+              echo "data ke: ".$k. " : ".$data[$k]." < "."data ke: ".($k-1). " : ".$data[$k-1]." = tukar";
               $dummy=$data[$k];
               $data[$k]=$data[$k-1];
               $data[$k-1]=$dummy;
@@ -21,7 +21,7 @@ echo "<br>";
             }
             else
             {
-              echo "data ke: ".$k. " : ".$data[$k]." < "."data ke: ".($k-1). " : ".$data[$j-1]." = tidak tukar"; 
+              echo "data ke: ".$k. " : ".$data[$k]." < "."data ke: ".($k-1). " : ".$data[$k-1]." = tidak tukar"; 
             }
       } 
       print_r($data);
