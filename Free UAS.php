34634033b444fde33d99bea6cb93e89e8b52198f
AndyRachmat27 <80530325+AndyRachmat27@users.noreply.github.com>
2021-06-22 21:57:17 +0700

    Update Free UAS.php

--- Free UAS.php
+++ Free UAS.php
@@ -1,7 +1,11 @@
 <?php
 
 $teks = "Nvidia dikabarkan akan mengurangi produksi kartu grafis lamanya untuk bisa meningkatkan volume produksi lini kartu grafis terbarunya, RTX 3000. Dilansir Techspot, Selasa, hal ini diketahui dari postingan di sebuah forum Board Channel, yang merupakan forum tertutup yang berisi distributor dan direksi rekanan pembuat kartu grafis.Dalam sebuah postingan di forum tersebut diketahui kalau Nvidia mengirimkan pemberitahuan pada perusahaan pembuat kartu grafis kalau mereka akan mengurangi pasokan chip RTX 2060 sampai setengahnya mulai bulan Juni ini.";
-$kata   = explode(" ", $teks);
+$filter = array(".",",","?","!","(",")","-","&","+","_","=",":");
+
+$teksfilter = str_replace($filter, "", $teks);
+
+$kata   = explode(" ", $teksfilter);
 $hasil  = count($kata);
 $data   = array_count_values($kata);
 echo "<h3>Teks</h3> <br> $teks";
